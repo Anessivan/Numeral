6c57ec3dce25d6611a36d8584f328dfbd154ec75
Anessivan <Anessivan@mail.ru>
2020-12-19 14:37:34 +0300

    add sort

--- Numeral.cpp
+++ Numeral.cpp
@@ -1,19 +1,35 @@
  
 #include <cmath>
 #include <iostream> 
+#include <fstream>
  #include <omp.h>
  
+
+void bottle_incr(int* point, int size)
+{
+	int i = 0, j = 0;
+	int c = 0;
+	for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - 1; j++)
+			if (point[j] > point[j + 1])
+			{
+				c = point[j];
+				point[j] = point[j + 1];
+				point[j + 1] = c;
+			}
+}
+
  int* Find( int N)
  {
      int* temp = new int [N];
      int k = 0;
-     int num = 4;
+     int num = 8;
     omp_set_num_threads(num);
     double t1=omp_get_wtime();
     
 #pragma omp parallel for
     
-     for (int i = 2; i <= N; i++)
+     for (int i = 3; i <= N; i +=2)
      {
          for (int j = 2; j * j < i; j++)
          {
@@ -23,27 +39,32 @@
              {
                  temp[k] = i;
                  k++;
-                 std::cout << i << "\n";
+                 //std::cout << i << "\n";
              }
          }
      }
-     double t2=omp_get_wtime();     
+    double t2=omp_get_wtime();     
     int* res = new int[k];
     for(int i = 0; i < k; i++)
         res[i] = temp[i];
+	bottle_incr(res, k);
+	std::ofstream out;
+	out.open("../Numeral.txt");
+	out << 2 << "\n" << 3 << "\n";
     for (int i = 0; i < k; i++)
-        std::cout << res[i] << std::endl;
+        out << res[i] << "\n";
     std::cout<<"num threads: "<< num <<std::endl;
     std::cout<<"time: "<<t2-t1<<"s"<<std::endl;
     delete[] temp;
     return res;
  }
  
+ 
  int main()
  {
      int* numeral;
-     numeral = Find(30);
-     delete[] numeral;
+     numeral = Find(1000);
+	 delete[] numeral;
      return 0;
  }
  
